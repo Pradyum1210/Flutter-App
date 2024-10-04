5de0f5b41811d64eac01db0df33a293a58c7cf38
Pradyum1210 <mohalepradyumna@gmail.com>
2024-10-05 01:08:36 +0530

    Application

--- Class App/StatelessWidget Apps/column_with_scroll/lib/main.dart
+++ Class App/StatelessWidget Apps/column_with_scroll/lib/main.dart
@@ -30,13 +30,13 @@ class MainApp extends StatelessWidget{
                   width: 200,
                   color: Colors.amber,
                 ),
-                Image.network("https://thumbs.dreamstime.com/b/environment-earth-day-hands-trees-growing-seedlings-bokeh-green-background-female-hand-holding-tree-nature-field-gra-130247647.jpg",),
+                Image.network("https://images.unsplash.com/photo-1620435363451-b84b06e6de72?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bmF0dXJlJTIwaW1hZ2VzfGVufDB8fDB8fHww",),
                 Container(
                   height: 200,
                   width: 200,
                   color: Colors.amber,
                 ),
-                Image.network("https://thumbs.dreamstime.com/b/environment-earth-day-hands-trees-growing-seedlings-bokeh-green-background-female-hand-holding-tree-nature-field-gra-130247647.jpg",),
+                Image.network("https://media.istockphoto.com/id/517188688/photo/mountain-landscape.jpg?s=612x612&w=0&k=20&c=A63koPKaCyIwQWOTFBRWXj_PwCrR4cEoOw2S9Q7yVl8=",),
                 Container(
                   height: 200,
                   width: 200,
